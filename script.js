2e53f7bf765f2e183e57ac7082397afaf97e09f6
Binay06 <141115355+Binay06@users.noreply.github.com>
2024-08-17 23:58:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var currentYear = new Date().getFullYear();
+        document.querySelector('footer p').innerHTML = '&copy; ' + currentYear + ' All Rights are Reserved Technical Support by NIC and Content Management by Commissionerate Police, Bhubaneswar-Cuttack';
+    });
